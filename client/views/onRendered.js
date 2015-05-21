4638d6a0b198719ae37c54347495548e77c64170
Matt Krick <matt.krick@gmail.com>
2015-05-21 16:04:37 -0500

    add net

--- client/views/onRendered.js
+++ client/views/onRendered.js
@@ -1,86 +1,40 @@
-Template.lineChart.onRendered(function() {
-    //Width and height
-    var margin = {top: 20, right: 20, bottom: 30, left: 50},
-        width = 600 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-    var x = d3.time.scale()
-        .range([0, width]);
-
-    var y = d3.scale.linear()
-        .range([height, 0]);
-
-    var xAxis = d3.svg.axis()
-        .scale(x)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        .scale(y)
-        .orient("left");
-
-    var line = d3.svg.line()
-        .x(function (d) {
-            return x(d.date);
-        })
-        .y(function (d) {
-            return y(d.value);
-        });
-
-    var svg = d3.select("#lineChart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")");
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Latidos por minuto");
-
-    Tracker.autorun(function () {
-        var dataset = Points.find({}, {sort: {date: -1}, limit: 20}).fetch();
-
-        var paths = svg.selectAll("path.line")
-            .data([dataset]);
-
-        x.domain(d3.extent(dataset, function (d) {
-            return d.date;
-        }));
-        y.domain(d3.extent(dataset, function (d) {
-            return d.value;
-        }));
-
-        //Update X axis
-        svg.select(".x.axis")
-            .transition()
-            .duration(1000)
-            .call(xAxis);
-
-        //Update Y axis
-        svg.select(".y.axis")
-            .transition()
-            .duration(1000)
-            .call(yAxis);
-
-        paths
-            .enter()
-            .append("path")
-            .attr("class", "line")
-            .attr('d', line);
-
-        paths
-            .attr('d', line);
-
-        paths
-            .exit()
-            .remove();
-    });
+Template.lineChart.onRendered(function () {
+  //this.autorun(function (comp) {
+  //  if (!Points.find().count()) return;
+  //myChart.push([{time: 5, y:5}]);
+  //comp.stop();
+  //});
+  //var colCount = Points.find({},{reactive: false}).count();
+  var first = true;
+  Points.find().observeChanges({
+    added: function (id, fields) {
+      console.log('just added: ',fields.time);
+      if (first) {
+        var lineChartData = [
+          {
+            label: "Series 1",
+            values: [fields]
+          }];
+        myChart = $('#myChart').epoch(
+          {
+            type: 'time.line',
+            data: lineChartData,
+            axes: ['left', 'bottom', 'right'],
+            windowSize: 10,
+            ticks: {time: 1, right: 5, left: 5},
+            queueSize: 1,
+            tickFormats: {
+              bottom: function (d) {
+                console.log('just postd: ', d);
+                return d.getSeconds();
+              }
+            }
+          });
+        first = false;
+      } else {
+        myChart.push([fields]);
+      }
+    }
+  })
 });
+
--- server/cardio.js
+++ server/cardio.js
@@ -1,6 +1,54 @@
-Meteor.setInterval(function() {
-    Points.insert({
-        date: new Date(),
-        value:Math.floor(Math.random() * 60)+40
-    });
-},1000);
+//var RealTimeData = function(layers) {
+//  this.layers = layers;
+//  this.timestamp = ((new Date()).getTime() / 1000)|0;
+//};
+//
+//RealTimeData.prototype.rand = function() {
+//  return parseInt(Math.random() * 100) + 50;
+//};
+//
+//RealTimeData.prototype.history = function(entries) {
+//  if (typeof(entries) != 'number' || !entries) {
+//    entries = 60;
+//  }
+//
+//  var history = [];
+//  for (var k = 0; k < this.layers; k++) {
+//    history.push({ values: [] });
+//  }
+//
+//  for (var i = 0; i < entries; i++) {
+//    for (var j = 0; j < this.layers; j++) {
+//      history[j].values.push({time: this.timestamp, y: this.rand()});
+//    }
+//    this.timestamp++;
+//  }
+//
+//  return history;
+//};
+//
+//RealTimeData.prototype.next = function() {
+//  var entry = [];
+//  for (var i = 0; i < this.layers; i++) {
+//    entry.push({ time: this.timestamp, y: this.rand() });
+//  }
+//  this.timestamp++;
+//  return entry;
+//};
+//
+////window.RealTimeData = RealTimeData;
+//
+//meteor
+
+Meteor.methods({
+  'startNet': function (port) {
+    var net = Meteor.npmRequire('net');
+    net.createServer(function (socket) {
+      console.log("connected");
+
+      socket.on('data', function (data) {
+        console.log(data.toString());
+      });
+    }).listen(8124);
+  }
+});
