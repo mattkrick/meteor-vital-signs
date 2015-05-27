825acb3c92d979280b36b0b46442aebf3e82794b
mattkrick <matt.krick@gmail.com>
2015-05-27 10:47:07 -0500

    add sensor map

--- server/cardio.js
+++ server/cardio.js
@@ -39,12 +39,12 @@
 ////window.RealTimeData = RealTimeData;
 //
 //meteor
-Meteor.setInterval(function() {
-  Points.insert({
-    time: new Date(),
-    y:Math.floor(Math.random() * 60)+40
-  });
-},1000);
+//Meteor.setInterval(function() {
+//  Points.insert({
+//    time: new Date(),
+//    y:Math.floor(Math.random() * 60)+40
+//  });
+//},1000);
 
 Meteor.methods({
   'startNet': function (port) {
@@ -53,8 +53,56 @@ Meteor.methods({
       console.log("connected");
 
       socket.on('data', function (data) {
-        console.log(data.toString());
+        var myData = getValues(data);
+        console.log(myData);
       });
     }).listen(8124);
   }
 });
+
+function getValues(data){
+  var myData={
+    temperature: 0,
+    pressure: 0,
+    flowRate: 0,
+    etCO2: 0,
+    inspiredCO2: 0,
+    breathePerMinute: 0,
+    reserved: 0,
+    CO2curve: []
+  };
+  var temp=0;
+  for(var index=0; index<data.length; index++){
+    if((data[index]&0x80)==128){
+      temp=(data[index+1]&0x7F)+((data[index]&0x03)<<8)+((data[index]&0x04)<<7);
+
+      switch((data[index]&0x78)>>3){
+        case 1:
+          myData.temperature=temp;
+          break;
+        case 2:
+          myData.pressure=temp;
+          break;
+        case 12:
+          myData.flowRate=temp;
+          break;
+        case 0:
+          myData.etCO2=temp;
+          break;
+        case 13:
+          myData.inspiredCO2=temp;
+          break;
+        case 6:
+          myData.breathePerMinute=temp;
+          break;
+        case 9:
+          myData.reserved=temp;
+          break;
+        case 5:
+          myData.CO2curve.push(temp);
+          break;
+      }
+    }
+  }
+  return myData;
+}
